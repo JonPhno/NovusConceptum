f2d89bfe32865764252e8eab7c3fae5b1275ba18
William Pitre <william.pitre97@hotmail.com>
2016-09-16 14:52:06 -0400

    page d'acceuil + debut a propos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace NovusConceptum.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Description du club NovusConceptum";
 
             return View();
         }
